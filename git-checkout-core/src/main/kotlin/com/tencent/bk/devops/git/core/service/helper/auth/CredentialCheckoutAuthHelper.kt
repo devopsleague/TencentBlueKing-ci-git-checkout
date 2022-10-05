2cf208eda80f5eda42d7333adeafba126da690e9
mingshewhe <mingshewhe@tencent.com>
2022-10-05 22:59:26 +0800

    feat: 新增basic auth授权方式

--- git-checkout-core/src/main/kotlin/com/tencent/bk/devops/git/core/service/helper/auth/CredentialCheckoutAuthHelper.kt
+++ git-checkout-core/src/main/kotlin/com/tencent/bk/devops/git/core/service/helper/auth/CredentialCheckoutAuthHelper.kt
@@ -248,8 +248,11 @@ class CredentialCheckoutAuthHelper(
         commands: MutableList<String>
     ) {
         if (git.isAtLeastVersion(GitConstants.SUPPORT_EMPTY_CRED_HELPER_GIT_VERSION)) {
-            commands.add("git config --add credential.helper '' ")
+            commands.add("git config --add credential.helper \"\" ")
         }
-        commands.add("git config --add credential.helper '!bash $credentialShellPath ${settings.pipelineTaskId}'")
+        commands.add(
+            "git config --add credential.helper " +
+                "\"!bash ${File(credentialShellPath).canonicalPath} ${settings.pipelineTaskId}\""
+        )
     }
 }
